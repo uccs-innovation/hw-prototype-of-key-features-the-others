a8ac1cad5747b2c51a7836b7bf6de9515c06275d
shreyamadiga <90430614+shreyamadiga@users.noreply.github.com>
2022-09-25 17:58:18 -0600

    //my feature

---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace DXMauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+    public class Employee
+    {
+        public string Name { get; set; }
+       
+        public bool OnLeave { get; set; }
+    }
+}
